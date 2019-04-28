d5e8b7236926abc268ba9ba616ce07a17882083c
DESKTOP-M51TMTU\Tejaswi <tejaswi.gillepally@gmail.com>
2019-04-27 22:42:31 -0400

    This is my First commit

---
+++ src/main/java/com/teju/studentMaster/StudentMasterApplication.java
@@ -0,0 +1,13 @@
+package com.teju.studentMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMasterApplication.class, args);
+	}
+
+}
